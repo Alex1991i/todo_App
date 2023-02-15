29e765d22cf9c7391f9f0c44d2e010d2a21c9b60
Alex Kudrinskiy <Firefly1991i@yandex.ru>
2023-02-15 20:26:57 +0300

    validation

--- src/components/newTaskForm/newTaskForm.js
+++ src/components/newTaskForm/newTaskForm.js
@@ -1,10 +1,12 @@
-import { Component } from 'react';
+import { Alert } from 'antd';
+import React, { Component } from 'react';
 
 export default class NewTaskForm extends Component {
   state = {
     label: '',
     minute: '',
     seconds: '',
+    error: false,
   };
 
   onLabelChange = (e) => {
@@ -24,40 +26,51 @@ export default class NewTaskForm extends Component {
           label: '',
           minute: '',
           seconds: '',
+          error: false,
+        });
+      } else {
+        this.setState({
+          error: true,
         });
       }
     }
   };
 
   render() {
-    const { label, minute, seconds } = this.state;
+    const { label, minute, seconds, error } = this.state;
+    const err = error ? <Alert type="error" message="Введены невалидные данные" /> : null;
+
     return (
-      <form className="header new-todo-form" onKeyDown={this.onKeyDown}>
-        <h1>todos</h1>
-        <input
-          className="new-todo"
-          placeholder="Task"
-          // eslint-disable-next-line jsx-a11y/no-autofocus
-          autoFocus
-          onChange={this.onLabelChange}
-          value={label}
-          name="label"
-        />
-        <input
-          className="new-todo-form__timer"
-          placeholder="Min"
-          name="minute"
-          onChange={this.onLabelChange}
-          value={minute}
-        />
-        <input
-          className="new-todo-form__timer"
-          placeholder="Sec"
-          name="seconds"
-          onChange={this.onLabelChange}
-          value={seconds}
-        />
-      </form>
+      // eslint-disable-next-line react/jsx-fragments
+      <React.Fragment>
+        <form className="header new-todo-form" onKeyDown={this.onKeyDown}>
+          <h1>todos</h1>
+          <input
+            className="new-todo"
+            placeholder="Task"
+            // eslint-disable-next-line jsx-a11y/no-autofocus
+            autoFocus
+            onChange={this.onLabelChange}
+            value={label}
+            name="label"
+          />
+          <input
+            className="new-todo-form__timer"
+            placeholder="Min"
+            name="minute"
+            onChange={this.onLabelChange}
+            value={minute}
+          />
+          <input
+            className="new-todo-form__timer"
+            placeholder="Sec"
+            name="seconds"
+            onChange={this.onLabelChange}
+            value={seconds}
+          />
+        </form>
+        {err}
+      </React.Fragment>
     );
   }
 }
